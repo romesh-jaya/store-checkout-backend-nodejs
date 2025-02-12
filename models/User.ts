2e2a9e47e25a710aedf85445ac642a626eea2fac
Romesh <romesh@medoment.com>
2025-02-12 15:17:21 +0530

    1

---
+++ models/User.ts
@@ -0,0 +1,8 @@
+export type User = {
+  id?: string;
+  email: string;
+  password_hash: string;
+  is_admin?: boolean;
+  created_at?: string;
+  updated_at?: string;
+};
--- routes/v1/auth.ts
+++ routes/v1/auth.ts
@@ -1,10 +1,55 @@
 export {};
 
 const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs');
 import { Request as JWTRequest } from 'express-jwt';
 const checkAuth = require('../../middleware/check-auth');
 import express from 'express';
+import { UserModel } from '../../database/models/user.model';
+import { User } from '../../models/User';
 const router = express.Router();
+
+router.post('/signup', (req, res) => {
+  bcryptjs.hash(req.body.password, 10).then((hash) => {
+    const userInput: User = {
+      email: req.body.email,
+      password_hash: hash,
+      is_admin: false,
+    };
+
+    UserModel.create(userInput)
+      .then((user) => {
+        const token = jwt.sign({ id: user.id }, process.env.HASHSECRET, {
+          expiresIn: process.env.TOKENEXPIRATION,
+        });
+        const refreshToken = jwt.sign({ id: user.id }, process.env.HASHSECRET, {
+          expiresIn: process.env.REFRESHTOKENEXPIRATION,
+        });
+        res.status(201).json({
+          message: 'User created!',
+          token: token,
+          isAdmin: false,
+          refreshToken: refreshToken,
+        });
+      })
+      .catch((error) => {
+        if (error.name && error.name == 'SequelizeUniqueConstraintError') {
+          if (error.errors && error.errors.length > 0) {
+            const firstError = error.errors[0];
+            if (firstError.path && firstError.path == 'email') {
+              return res.status(500).json({
+                message: 'Email already exists',
+              });
+            }
+          }
+        }
+
+        res.status(500).json({
+          message: error.message,
+        });
+      });
+  });
+});
 
 router.post('/login', (req, res) => {
   const token = jwt.sign({ id: 1 }, process.env.HASHSECRET, {
